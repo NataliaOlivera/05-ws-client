59d0806972e76957792c97488b068d3c54ddbe29
NataliaOlivera <nati.220997@gmail.com>
2023-05-10 10:48:50 -0300

    websocket

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,40 @@
+import { connectToServer } from './socket-client'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+
+    <h2> Websocket - Client</h2>
+
+    <input id="jwt-token" placeholder="Json Web Token"/>
+
+    <button id="btn-connect">Connect</button>
+
+    <br>
+
+    <span id="server-status">offline</span>
+
+    <ul id='clientes-ul'></ul>
+
+    <form id="message-form">
+      <input placeholder="menssage" id="message-input"/>
+    </form>
+
+    <h3>Messages</h3>
+    <ul id="messages-ul"></ul>
+
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// connectToServer(); // 
+
+const jwtToken = document.querySelector<HTMLInputElement>('#jwt-token')!;
+const btnConnect = document.querySelector<HTMLButtonElement>('#btn-connect')!;
+
+btnConnect.addEventListener('click', () => {
+
+  if( jwtToken.value.trim().length <=0 ) return alert('Enter a valid JWT');
+  connectToServer( jwtToken.value.trim() );
+  
+});
